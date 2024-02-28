3d559dbb31b8c0e147acc487d236bf36285a5a99
Ian Coelho <ian.coel@hotmail.com>
2024-02-27 21:46:52 -0300

    feat: Adds letter counter

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ def main():
 
   with open(book_path) as file:
     complete_file = file.read()
-    complete_file_splitted = complete_file.split()
-    print(len(complete_file_splitted))
+    letter_count = {}
+
+    for letter in complete_file:
+      normalized_letter = letter.lower()
+      count = letter_count.get(normalized_letter)
+      if count:
+        letter_count[normalized_letter] += 1
+      else:
+        letter_count[normalized_letter] = 1
+
+    print(letter_count)
 
 if __name__ == "__main__":
   main()
